4007db74af3c986b0d2fa8d7c9feb3a76514967d
MatiasAndrada <matiasandrada5622@gmail.com>
2026-08-25 21:28:16 -0300

    feat(database): add deleted_at field to business records

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -86,6 +86,7 @@ export type Database = {
         Row: {
           business_id: string
           created_at: string
+          deleted_at: string | null
           description: string | null
           id: string
           image_urls: string[] | null
@@ -97,6 +98,7 @@ export type Database = {
         Insert: {
           business_id: string
           created_at?: string
+          deleted_at?: string | null
           description?: string | null
           id?: string
           image_urls?: string[] | null
@@ -108,6 +110,7 @@ export type Database = {
         Update: {
           business_id?: string
           created_at?: string
+          deleted_at?: string | null
           description?: string | null
           id?: string
           image_urls?: string[] | null
